17930d1c6543248be489643e831974575695fd51
Daniel O'Leary <djolear@stanford.edu>
2020-04-25 17:28:25 -0700

    cleaned up

--- nadm1_hei.R
+++ nadm1_hei.R
@@ -12,12 +12,17 @@ SFATMIN=8
 SFATMAX=16
 ADDSUGMIN=6.5
 ADDSUGMAX=26
+SODMIN=1.1;
+SODMAX=2.0;
+RGMIN=1.8;
+RGMAX=4.3;
+SFATMIN=8;
+SFATMAX=16;
 
-asa_24_total_hei <-
+hei <-
   asa_24_total %>% 
-  filter(KCAL > 0) %>% 
-  filter(complete == "Y") %>% 
-  filter(subid %!in% low_cals$subid) %>%   
+  mutate(subid = subid.x) %>% 
+  filter(KCAL > 1000) %>% 
   mutate(
     VEGDEN = ( V_TOTAL + V_LEGUMES ) / (KCAL / 1000),
     HEI2015C1_TOTALVEG=5*(VEGDEN/1.1),
@@ -75,17 +80,24 @@ asa_24_total_hei <-
 
                       
       
-    SODDEN  = SODI / (KCAL / 1000),
+    SODDEN  = SODI / (KCAL),
     HEI2015C10_SODIUM = 
       ifelse(SODDEN <= SODMIN, 10, 
-             ifelse(SODDEN >= SODMAX, 0, 10 * (SODDEN - SODMIN) / (SODMAX - SODMIN))
+             ifelse(SODDEN >= SODMAX, 0, (10 * (SODDEN - SODMIN) / (SODMAX - SODMIN)))
       ),
     
     RGDEN = G_REFINED / (KCAL / 1000),
-    
+    HEI2015C11_REFINEDGRAIN = 
+      ifelse(RGDEN <= RGMIN, 10, 
+             ifelse(RGDEN >= RGMAX, 0, (10 - ( 10* (RGDEN-RGMIN) / (RGMAX-RGMIN) )))
+      ),
+
     
     SFAT_PERC = 100 * (SFAT * 9 / KCAL),
-    
+    HEI2015C12_SFAT = 
+      ifelse(SFAT_PERC >= SFATMAX, 0, 10 - ( 10* (SFAT_PERC-SFATMIN) / (SFATMAX-SFATMIN))
+    ),
+
 
     ADDSUG_PERC = 100 * (ADD_SUGARS * 16 / KCAL), 
     HEI2015C13_ADDSUG = 
@@ -93,22 +105,63 @@ asa_24_total_hei <-
              ifelse(ADDSUG_PERC >= ADDSUGMAX, 0, 10 * (ADDSUG_PERC - ADDSUGMIN) / (ADDSUGMAX - ADDSUGMIN))
       ),
     
-    hei = HEI2015C1_TOTALVEG + HEI2015C2_GREEN_AND_BEAN + HEI2015C3_TOTALFRUIT + HEI2015C4_WHOLEFRUIT + HEI2015C5_WHOLEGRAIN + 
-      HEI2015C6_TOTALDAIRY + HEI2015C7_TOTPROT + HEI2015C8_SEAPLANT_PROT + HEI2015C9_FATTYACID,
+    hei = HEI2015C1_TOTALVEG + HEI2015C2_GREEN_AND_BEAN + HEI2015C3_TOTALFRUIT + HEI2015C4_WHOLEFRUIT + 
+      HEI2015C5_WHOLEGRAIN + HEI2015C6_TOTALDAIRY + HEI2015C7_TOTPROT + HEI2015C8_SEAPLANT_PROT + 
+      HEI2015C9_FATTYACID + HEI2015C10_SODIUM + HEI2015C11_REFINEDGRAIN + HEI2015C12_SFAT + HEI2015C13_ADDSUG,
+    
+    VEGDEN = ( V_TOTAL ) / (KCAL / 1000),
+    VGRNDEN = ( V_DRKGR ) / (KCAL / 1000),
+    FDEN = ( F_TOTAL ) / (KCAL / 1000),
+    SUGDEN = ( ADD_SUGARS ) / (KCAL / 1000),
+    GDEN = ( G_WHOLE ) / (KCAL / 1000),
+    GREFDEN = ( G_REFINED ) / (KCAL / 1000),
+    SFDEN = ( SOLID_FATS ) / (KCAL / 1000),
+    CHEESEDEN = ( D_CHEESE ) / (KCAL / 1000),
+    NUTDEN = ( PF_NUTSDS ) / (KCAL / 1000),
+    he = ( scale(VEGDEN) + scale(FDEN) - scale(SUGDEN) + scale(GDEN) - scale(SFDEN) ) /5
              
   ) %>% 
-  select(
-    subid,
+  dplyr::select(
+    subid ,
+    ReportingDate,
     health_lm_beta,
     health_glm_beta,
-    ses_comp_all,
-    ses_subj_all,
-    ladd_comm,
+    ses_subj_all2,
+    hese,
     effexp,
     stai_trait,
+    pss,
+    cesd,
+    panas_neg,
+    erq_reap,
+    erq_10,
+    hei,
+    he,
+    gender,
+    veg,
+    neg,
+    bmi,
+    tfeq_cr,
+    eth,
     VEGDEN:HEI2015C13_ADDSUG,
-    hei
+    AmtUsual
+
+  )  %>% 
+  mutate(
+    wday = as.factor(lubridate::wday(lubridate::mdy(stringr::str_replace(stringr::str_replace(ReportingDate, "/", "-"), "/", "-"))))
   )
 
-lm1 <- lmer(HEI2015C2_GREEN_AND_BEAN ~ ladd_comm + (1|subid), data = asa_24_total_hei)
+hei %>% 
+  cor(., use = "pairwise.complete.obs") %>% 
+  round(., 2)
+
+hei <- 
+  hei %>% 
+  left_join(
+    ssdm2_ddm
+  )
+
+lm1 <- lmer(hei ~ HealthWeight + as.factor(gender) + AmtUsual + (1|subid), data = hei)
+lm1 <- lm(hei ~ HealthWeight , data = hei)
+
 summary(lm1)
